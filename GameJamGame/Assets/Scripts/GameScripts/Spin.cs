6a321a7e68440c39a72e7b900d311f8b47fbbd4b
Norsefey <129870643+Norsefey@users.noreply.github.com>
2024-07-28 16:12:32 -0400

    Level Two Block out

--- GameJamGame/Assets/Scripts/GameScripts/Spin.cs
+++ GameJamGame/Assets/Scripts/GameScripts/Spin.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class Spin : MonoBehaviour
 {
     public float rotSpeed = 5;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
