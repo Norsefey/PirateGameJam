8757274063fb876c34ae4d28b7241e4515a552a5
Norsefey <129870643+Norsefey@users.noreply.github.com>
2024-07-28 18:20:14 -0400

    TTP

--- GameJamGame/Assets/Scripts/PlayerScripts/RearCameraToggle.cs
+++ GameJamGame/Assets/Scripts/PlayerScripts/RearCameraToggle.cs
@@ -8,12 +8,6 @@ public class RearCameraToggle : MonoBehaviour
 
     bool camON = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
